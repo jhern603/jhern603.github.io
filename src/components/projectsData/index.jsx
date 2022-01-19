6f37e8b62489a64101b9d1a69053d4f1c532ab3a
jhern603 <jhern603@gmail.com>
2022-01-19 16:44:52 -0500

    further work on projects section

--- src/components/projectsData/index.jsx
+++ src/components/projectsData/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import data from '../../pages/context.json'
+import data from '../../pages/context.json';
 
 const ProjectsData = () => {
   return data.Projects.map((project) => {
     return (
-      <li
-        className={`test ${
-          project.id % 2 === 1 ? 'project__item__even' : 'project__item '
-        }`}
-      >
+      <li className={`project__item${project.id % 2 === 0 ? '__even' : ''}`}>
         <img src={project.image} alt="" className="project__image" />
         <div className="right">
           <header className="project__header">{project.name}</header>
           <span className="project__description">{project.description}</span>
+          <br />
+          <br />
+          <br />
+          <a
+            href={project.link}
+            className="view__btn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on GitHub
+          </a>
         </div>
       </li>
     );
