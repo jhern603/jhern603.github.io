78c2a454249d43f6ad5dd87998a4f4df1bccdf93
Jose Hernandez <jhern603@fiu.edu>
2022-01-21 19:24:35 -0500

    Fixed styling on mobile

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import data from '../context.json'
+import data from '../context.json';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
     <section className="home__container" id="home">
       <div className="hero">
         <h1 className="hero__header">{data.name}</h1>
-        <p className="hero__blurb">{data.hero}</p>
+        <Typewriter
+          options={{ wrapperClassName:"hero__blurb" }}
+          onInit={(typewriter) => {
+            typewriter.changeDelay(30).typeString(data.hero).start();
+            
+          }}
+        />
       </div>
     </section>
   );
