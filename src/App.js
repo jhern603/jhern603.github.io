02d5d5dfe2edc5f5b7d26dfd4e100b865a83657a
Jose Hernandez <jhern603@fiu.edu>
2022-01-30 17:13:08 -0500

    fireUwU

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { initializeApp } from 'firebase/app';
 import React from 'react';
 import Navbar from './components/navbar';
 import Home from './pages/home';
@@ -9,7 +10,15 @@ function App() {
   const home = React.useRef();
   const about = React.useRef();
   const projects = React.useRef();
-
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCAraiwf-Fk_DoiD1oUwGgoBBePv41gUgM',
+    authDomain: 'uwuportfolio.firebaseapp.com',
+    projectId: 'uwuportfolio',
+    storageBucket: 'uwuportfolio.appspot.com',
+    messagingSenderId: '923273703749',
+    appId: '1:923273703749:web:6f79b9aa75ca2334e80e2c',
+  };
+  const app = initializeApp(firebaseConfig);
   return (
     <div className="App">
       <Navbar />
@@ -18,7 +27,7 @@ function App() {
       <Projects ref={projects} />
       <p className="footer">
         &copy; Jose "UWU" Hernandez 2020 -{' '}
-        <Link to="home" smooth={true} duration={500} className = "back_to_top">
+        <Link to="home" smooth={true} duration={500} className="back_to_top">
           Back to UWU
         </Link>
       </p>
