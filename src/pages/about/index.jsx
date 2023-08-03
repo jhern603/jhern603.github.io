a0a9f6d96e1fd5e8056c544291579d1467616a83
jhern603 <jhern603@gmail.com>
2023-08-02 21:06:32 -0500

    Added new OCI badge

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import data from '../context.json';
 import portrait from './images/portrait.jpg';
-import badge from './images/infrastructure_badge.png';
+import foundations_badge from './images/infrastructure_badge.png';
+import dev_badge from './images/dev_badge.png';
 
 const About = () => {
   return (
@@ -11,15 +12,25 @@ const About = () => {
         <div className="left">
           <div className="portrait__container">
             <img src={portrait} className="portrait" alt="" />
+            <div className="badge__container">
+              <a
+                className="badge__container"
+                href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=4FCDC324F5F59B1923A58A7512DE95D8418DC01CF7FBD4447911D6C336183A8C"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={foundations_badge} className="badge" />
+              </a>
+              <a
+                className="badge__container"
+                href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=7B6BF26ED1A29B37C740942642FB7180BCA7A79B18CC8F1507AF5CDE41B3451C"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={dev_badge} className="badge" />
+              </a>
+            </div>
           </div>
-          <a
-            className="badge__container"
-            href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=4FCDC324F5F59B1923A58A7512DE95D8418DC01CF7FBD4447911D6C336183A8C"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={badge} className="badge" />
-          </a>
         </div>
         <div className="right">
           <p className="blurb">{data.blurb}</p>
